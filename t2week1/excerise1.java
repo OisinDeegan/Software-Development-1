e4c90e20616db713a3810a642deb6707cfa912eb
G00465926 Last name <G00465926@atu.ie>
2025-01-20 11:52:32 +0000

    lab 1

---
+++ t2week1/excerise1.java
@@ -0,0 +1,17 @@
+package t2week1;
+
+public class excerise1 {
+    public static void main(String[] args) {
+        String college = "ATU";
+        String module = "Software Development 1";
+        String course = "Computing and Digital Media";
+        int year = 1;
+
+        System.out.println("+---------------------------------------+");
+        System.err.println("| College: "+college+"\t\t\t\t|");
+        System.err.println("| Course: "+course+"\t|");
+        System.err.println("| Module: "+module+"\t|");
+        System.err.println("| Year: "+year+"\t\t\t\t|");
+        System.out.println("+---------------------------------------+");
+    }
+}
